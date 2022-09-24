a9c6ac299ed4330afa921641308a0ae94da02bfc
Madhukrishna Nipankar <nipankarmadhu@gmail.com>
2022-09-24 15:12:42 +0530

    serving static files

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const userRouter = require("./routes/userRoutes");
 
 // Middleware for putting body data to request object
 app.use(express.json());
+app.use(express.static(`${__dirname}/public`));
 
 // routes
 app.use("/api/v1/users", userRouter);
